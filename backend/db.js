e237b6242c5a6ee667ce5c781aa978cd8b04a25b
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-12-11 19:58:53 -0500

    added fix for database and redux toolkit

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    const connect = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDb connected: ${connect.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
