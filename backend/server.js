cd558e2a64dabec50824121f2ac14df6937589b7
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-12-14 22:06:52 -0500

    added single blog screen and more

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,26 @@ app.use(
 
 app.use("/api/blog-posts", blogPostRoutes);
 app.use("/api/users", userRoutes);
+app.get("/api/config/emailjs", (req, res) =>
+  res.send({
+    template_id: process.env.TEMPLATE_ID,
+    service_id: process.env.SERVICE_ID,
+    public_key: process.env.PUBLIC_KEY,
+  })
+);
 
-const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const port = process.env.PORT || 5005;
 
 app.listen(port, () => {
   console.log(`Server runs on port ${port}.`);
